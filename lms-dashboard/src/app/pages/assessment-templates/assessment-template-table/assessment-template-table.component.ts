def744f99831ee9ba54ef3efa1ce47c2bde0195f
Xavier Buillit <xavier@apithings.net>
2018-09-08 22:47:53 +0800

    DAS: Add share button with the qrcode generator

--- lms-dashboard/src/app/pages/assessment-templates/assessment-template-table/assessment-template-table.component.ts
+++ lms-dashboard/src/app/pages/assessment-templates/assessment-template-table/assessment-template-table.component.ts
@@ -1,8 +1,86 @@
-import {Component} from '@angular/core';
-import {LocalDataSource} from 'ng2-smart-table';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {LocalDataSource, ViewCell} from 'ng2-smart-table';
 
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {AssessmentTemplatesService} from "../../../../lms-api/services";
+import {AssessmentTemplate} from "../../../../lms-api/classes";
+import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {environment} from "../../../../environments/environment";
+
+
+@Component({
+  selector: 'ngx-view-assessment-template-generate-qrcode-modal',
+  template: `
+    <div class="modal-header">
+      <span>Generate QrCode</span>
+      <button class="close" aria-label="Close" (click)="closeModal()">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      {{ uri }}
+      <qrcode [qrdata]="uri" [size]="256" [level]="'M'"></qrcode>
+    </div>
+  `,
+})
+export class ViewAssessmentTemplateGenerateQrCodeModalComponent {
+  @Input() uri: string;
+
+  constructor(
+    private activeModal: NgbActiveModal,
+  ) { }
+
+  closeModal() {
+    this.activeModal.dismiss();
+  }
+}
+
+@Component({
+  selector: 'button-view',
+  template: `
+    <button (click)="onShare()">Share</button>
+  `,
+})
+export class ButtonViewComponent implements ViewCell, OnInit {
+  renderValue: string;
+
+  @Input() value: string | number;
+  @Input() rowData: any;
+
+  @Output() save: EventEmitter<any> = new EventEmitter();
+
+  constructor(
+    private route: ActivatedRoute,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit() {
+    this.renderValue = this.value.toString().toUpperCase();
+  }
+
+  onShare() {
+    this.generateQrCode(this.rowData);
+  }
+
+  public generateQrCode(assessmentTemplate: AssessmentTemplate) {
+    const activeModal = this.modalService.open(ViewAssessmentTemplateGenerateQrCodeModalComponent, { size: 'lg', container: 'nb-layout' });
+
+    console.log(this.buildSubscriptionUrl(assessmentTemplate));
+    activeModal.componentInstance.uri = this.buildSubscriptionUrl(assessmentTemplate);
+
+    activeModal.result.then((result) => {
+      this.save.emit(this.rowData);
+    }, () => {
+    });
+  }
+
+  private buildSubscriptionUrl(assessmentTemplate: AssessmentTemplate) {
+    let url = environment.apiUrl.replace(/\/$/, "") + assessmentTemplate['@id'];
+
+    return url;
+  }
+}
+
 
 @Component({
   selector: 'ngx-assessment-template-table',
@@ -38,6 +116,15 @@ export class AssessmentTemplateTableComponent {
       name: {
         title: 'Name',
         type: 'string',
+      },
+      button: {
+        title: 'Button',
+        type: 'custom',
+        renderComponent: ButtonViewComponent,
+        onComponentInitFunction(instance) {
+          instance.save.subscribe(function (row) {
+          }.bind(this));
+        }
       },
     },
   };
--- lms-dashboard/src/app/pages/assessment-templates/assessment-templates.module.ts
+++ lms-dashboard/src/app/pages/assessment-templates/assessment-templates.module.ts
@@ -7,6 +7,11 @@ import {Ng2SmartTableModule} from "ng2-smart-table";
 import { NgJsonEditorModule } from 'ang-jsoneditor'
 import {SchemaFormModule} from "ngx-schema-form";
 import {AssessmentsService, AssessmentTemplatesService} from "lms-api/services";
+import {
+  ButtonViewComponent,
+  ViewAssessmentTemplateGenerateQrCodeModalComponent
+} from "./assessment-template-table/assessment-template-table.component";
+import {QRCodeModule} from "angularx-qrcode";
 
 @NgModule({
   imports: [
@@ -15,11 +20,16 @@ import {AssessmentsService, AssessmentTemplatesService} from "lms-api/services";
     Ng2SmartTableModule,
     NgJsonEditorModule,
     SchemaFormModule,
+    QRCodeModule,
   ],
   declarations: [
+    ButtonViewComponent,
+    ViewAssessmentTemplateGenerateQrCodeModalComponent,
     ...routedComponents,
   ],
   entryComponents: [
+    ButtonViewComponent,
+    ViewAssessmentTemplateGenerateQrCodeModalComponent,
   ],
   providers: [
     AssessmentTemplatesService,
