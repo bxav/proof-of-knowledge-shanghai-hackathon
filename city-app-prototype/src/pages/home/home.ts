5c35f9f37455bde564cb98c5341235358d77e768
Xavier Buillit <xavier@apithings.net>
2018-09-08 22:03:27 +0800

    APP: Add modal to enter address

--- city-app-prototype/src/pages/home/home.ts
+++ city-app-prototype/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) {
 
   }
 
+  public scanCode() {
+    if (true) { // if (!this.platform.is('cordova')) {
+      let alert = this.createNotAvailableAlert();
+      alert.present();
+    }
+  }
+
+
+  private createNotAvailableAlert() {
+    return this.alertCtrl.create({
+      title: 'Feature not available in browser',
+      subTitle: 'You have to enter the address of the assessment.',
+      inputs: [
+        {
+          name: 'address',
+          placeholder: 'Assessment'
+        }
+      ],
+      buttons: [{
+        text: 'Not Now',
+        role: 'cancel',
+        handler: () => {
+          this.navCtrl.popToRoot();
+        }
+      }, {
+        text: 'Submit',
+        role: '',
+        handler: (data) => {
+          return data;
+        }
+      }]
+    });
+  }
 }
