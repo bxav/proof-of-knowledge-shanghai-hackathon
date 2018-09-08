ebbd9eb634fc72f0e75833a3cb8cafff18d77e80
Xavier Buillit <xavier@apithings.net>
2018-09-08 19:46:26 +0800

    IoT: Add Nfc reader that lock or unlock on tap

--- iot-lock-prototype/src/pages/home/home.ts
+++ iot-lock-prototype/src/pages/home/home.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Ndef, NFC} from "@ionic-native/nfc";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  lock: boolean = true;
+export class HomePage implements OnInit {
+  public lock: boolean = true;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private nfc: NFC) {
+
+  }
+
+  ngOnInit () {
+    this.nfc.addNdefListener(() => {
+      console.log('successfully attached ndef listener');
+    }, (err) => {
+      console.log('error attaching ndef listener', err);
+    }).subscribe((event) => {
+      console.log('received ndef message. the tag contains: ', event.tag);
+      console.log('decoded tag id'+this.nfc.bytesToHexString(event.tag.id));
+
+      this.setLock(false);
+
+      return setTimeout(() => {
+        this.setLock(true);
+      },4000);
+    });
+  }
+
+  setLock(lock: boolean) {
+    this.lock = lock;
+
+    console.log('Lock' + this.lock);
   }
 }
